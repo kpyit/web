16e77bd2c17567674d5a81872891cdd8862c7bab
kpyit <kpy.it@mail.ru>
2024-01-29 21:16:04 +0300

    modified:   django/lesson_1/db.sqlite3 	modified:   django/lesson_1/myapp/models.py

--- django/lesson_1/myapp/models.py
+++ django/lesson_1/myapp/models.py
@@ -41,7 +41,7 @@ class Product(models.Model):
     description = models.TextField()
     price = models.DecimalField(max_digits=10, decimal_places=2)
     quantity = models.PositiveSmallIntegerField()
-    date_added = models.DateField('date added')
+    date_added = models.DateField(auto_now_add=True)#'date added'
 
     def __str__(self):
         return self.name
