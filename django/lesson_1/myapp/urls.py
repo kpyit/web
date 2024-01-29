a3d67c4e7e05acb3cb98259548d70ad8669f65d6
kpyit <kpy.it@mail.ru>
2024-01-29 21:00:07 +0300

    modified:   django/lesson_1/myapp/templates/homepage.html 	modified:   django/lesson_1/myapp/urls.py

--- django/lesson_1/myapp/urls.py
+++ django/lesson_1/myapp/urls.py
@@ -9,13 +9,13 @@ urlpatterns = [
     
     # lab2
     path('', homepage),
-    path('create_customer/', CustomerCreate.as_view()),
+    path('create_customer/', CustomerCreate.as_view(), name="create_customer"),
     path('<int:pk>/update_customer/', CustomerUpdate.as_view()),
     path('<int:pk>/delete_customer/', CustomerDelete.as_view()),
-    path('create_product/', ProductCreate.as_view()),
+    path('create_product/', ProductCreate.as_view(), name="create_product"),
     path('<int:pk>/update_product/', ProductUpdate.as_view()),
     path('<int:pk>/delete_product/', ProductDelete.as_view()),
-    path('create_order/', OrderCreate.as_view()),
+    path('create_order/', OrderCreate.as_view(), name="create_order"),
     path('<int:pk>/update_order/', OrderUpdate.as_view()),
     path('<int:pk>/delete_order/', OrderDelete.as_view()),  
 ] 
